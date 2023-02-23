d1ebd6034251f33f1b6c94a793152453644b3441
Michael Kawata <michaelkawata@bens-air.lan>
2023-02-22 16:59:00 -0800

    useeffect and mole timer

--- src/components/EmptySlot.js
+++ src/components/EmptySlot.js
@@ -1,7 +1,19 @@
+import { useEffect } from "react"
+import MoleHill from '../molehill.png'
+
 const EmptySlot = (props) => {
+
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 4000)
+        let timer = setTimeout(() => {
+            props.toggle(true)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+
     return (
         <div>
-            EmptySlot
+            <img style={{ 'width': '30vw' }} src={MoleHill} />
         </div>
     )
 }
